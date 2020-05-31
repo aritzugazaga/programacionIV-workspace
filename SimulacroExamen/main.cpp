7e0119820268273385168b3c173a69dccb5af3ab
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2020-05-31 10:47:12 +0200

    si

---
+++ SimulacroExamen/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 18 may. 2020
+ *      Author: aritz
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hola Mundo" << endl;
+
+	return 0;
+}
+
